43f816cb14bc40bb4939d9a3ce2601bea4b29523
David Barton <barton.david.sv@gmail.com>
2019-08-31 21:23:13 +0200

    adding better temporary content and more code

--- pixel-art-react-app/src/components/homeBody.jsx
+++ pixel-art-react-app/src/components/homeBody.jsx
@@ -1,8 +1,27 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import { Carousel1, Carousel2 } from './carousel'
+import BodyText from './bodyText'
 
 class HomeBody extends Component {
   render() { 
-    return <div className="bg-info"><div className="container"><div className="row"><img className="col" style={{ width: '150px' }} alt="pixel art" src="retro-gaming-with-laci.png"></img></div></div></div>
+    return (
+      <React.Fragment>
+        <div className="bg-warning">
+          <div className="container">
+            <div className="row">
+              <BodyText /><Carousel1 />
+            </div>
+          </div>
+        </div>
+        <div className="bg-info">
+          <div className="container">
+            <div className="row">
+              <Carousel2 /><BodyText />
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    )
   }
 }
  
--- pixel-art-react-app/src/index.js
+++ pixel-art-react-app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.css'
+import './pixel.css'
 import Wrapper from './components/wrapper'
 
-ReactDOM.render(<Wrapper />, document.getElementById('root'))
+ReactDOM.render(<Wrapper />, document.querySelector('#root'))

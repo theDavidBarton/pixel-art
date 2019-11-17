3824c9a97c1c0dd15f24cef8eaf98c46899368b3
David Barton <barton.david.sv@gmail.com>
2019-11-17 11:51:25 +0100

    fine tune pods

--- src/components/bodyText.jsx
+++ src/components/bodyText.jsx
@@ -1,15 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import Tags from './tags'
 
 class BodyText extends Component {
   render() {
     return (
-      <div className='col py-5 px-5'>
-        <h2>{this.props.data.h2}</h2>
-        <p className='lead'>{this.props.data.pLead}</p>
-        <p>{this.props.data.p}</p>
-        <Tags tags={this.props.tags} />
-      </div>
+      <Fragment>
+        {this.props.data.h2 ? (
+          <div className='col py-5 px-5'>
+            <h2>{this.props.data.h2}</h2>
+            <p className='lead'>{this.props.data.pLead}</p>
+            <p>{this.props.data.p}</p>
+            <Tags tags={this.props.tags} />
+          </div>
+        ) : null}
+      </Fragment>
     )
   }
 }

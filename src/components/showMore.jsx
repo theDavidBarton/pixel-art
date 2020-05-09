232c256a1f6d423e5e25f0c37297be771e0f873f
David Barton <barton.david.sv@gmail.com>
2020-05-09 11:10:04 +0200

    fix readmore alignment

--- src/components/showMore.jsx
+++ src/components/showMore.jsx
@@ -1,18 +1,17 @@
 import React, { Fragment } from 'react'
 
 export default function ShowMore({ onClick, dataLngth, displayedPosts }) {
+  const endOfPageReached = dataLngth < displayedPosts
+
   return (
     <Fragment>
-      <div>
-        <div className='container position-absolute text-sm-center'>
-          <button
-            onClick={onClick}
-            className='btn btn-dark position-absolute'
-            disabled={dataLngth < displayedPosts ? 'disabled' : null}
-            style={{ bottom: '30px' }}>
-            SHOW ME MORE!
-          </button>
-        </div>
+      <div className='bg-warning p-2'>
+        <button
+          onClick={onClick}
+          className={`btn ${endOfPageReached ? 'btn-secondary' : 'btn-info'} btn-lg btn-block`}
+          disabled={endOfPageReached ? 'disabled' : null}>
+          {endOfPageReached ? 'END OF PAGE' : 'SHOW ME MORE!'}
+        </button>
       </div>
     </Fragment>
   )

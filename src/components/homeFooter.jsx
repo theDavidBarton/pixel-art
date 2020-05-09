dad78299005feec1dedacde27f47a4d0a246c415
David Barton <43063460+theDavidBarton@users.noreply.github.com>
2020-05-09 11:11:30 +0200

    fix readmore alignment (#51)

--- src/components/homeFooter.jsx
+++ src/components/homeFooter.jsx
@@ -1,29 +1,25 @@
 import React from 'react'
 
 export default function HomeFooter() {
+  const currentYear = () => {
+    return new Date().getFullYear()
+  }
+
   return (
     <div className='bg-dark'>
       <div className='container text-warning text-center py-2'>
         <div>
           [
-          <a
-            href='https://github.com/theDavidBarton/pixel-art'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='text-info'>
+          <a href='https://github.com/theDavidBarton/pixel-art' target='_blank' rel='noopener noreferrer' className='text-info'>
             GitHub
           </a>
           ][
-          <a
-            href='https://www.linkedin.com/in/thedavidbarton/'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='text-info'>
+          <a href='https://www.linkedin.com/in/thedavidbarton/' target='_blank' rel='noopener noreferrer' className='text-info'>
             LinkedIn
           </a>
           ]
         </div>
-        <div>copyright © 2019-2020 theDavidBarton</div>
+        <div>copyright © 2019-{currentYear()} theDavidBarton</div>
       </div>
     </div>
   )

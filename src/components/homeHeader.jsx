885d3f481a99c8633415b092ccf99c74a029ff46
David Barton <43063460+theDavidBarton@users.noreply.github.com>
2020-09-30 11:59:03 +0200

    facelift sitebuild (#57)

--- src/components/homeHeader.jsx
+++ src/components/homeHeader.jsx
@@ -5,12 +5,12 @@ export default function HomeHeader() {
     <div className='bg-dark'>
       <div className='container'>
         <div className='text-right position-absolute mr-0 mt-0' style={{ right: 25, top: 5 }}>
-          <a href='https://github.com/theDavidBarton/pixel-art'>
+          <a href='https://github.com/theDavidBarton/pixel-art' target='_blank' rel='noopener noreferrer'>
             <img style={{ width: '17%' }} alt='github link' src='img/item-002.png'></img>
           </a>
         </div>
 
-        <div className='row text-warning py-4 px-4'>
+        <div className='row text-warning py-4 px-4 pointer-e-none'>
           <div>
             <div className='col lead d-sm-none'>_tDB's</div>
             <div className='col lead d-none d-sm-block'>_theDavidBarton's</div>

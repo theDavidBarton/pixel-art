270a6dfa616173fbcd05720c9e66a2a207c3cb01
theDavidBarton <theDavidBarton@users.noreply.github.com>
2021-09-21 12:47:06 +0000

    Deploying to gh-pages - 12:47:06

---
+++ precache-manifest.87a584b2ab63567263e0efc7cac293c7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e444b491347f17399542ce3cd3930121",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4053ac3d60ec967e3131",
+    "url": "./static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "e2889be4128ed30c81a6",
+    "url": "./static/css/main.d25b36e6.chunk.css"
+  },
+  {
+    "revision": "4053ac3d60ec967e3131",
+    "url": "./static/js/2.33593ee4.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "./static/js/2.33593ee4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2889be4128ed30c81a6",
+    "url": "./static/js/main.4e98ae9c.chunk.js"
+  },
+  {
+    "revision": "34bc3874b1144b1fdd6c",
+    "url": "./static/js/runtime-main.be550f74.js"
+  },
+  {
+    "revision": "5a94f70d047866186a5624c41eff46b6",
+    "url": "./static/media/ArcadeClassic.5a94f70d.otf"
+  }
+]);

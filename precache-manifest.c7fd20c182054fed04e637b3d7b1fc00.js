b6ff2a53102e7b03af41416c5db60f4d5b1ffaf8
theDavidBarton <theDavidBarton@users.noreply.github.com>
2023-01-06 20:22:53 +0000

    Deploying to gh-pages - 20:22:53

---
+++ precache-manifest.c7fd20c182054fed04e637b3d7b1fc00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b11f910079b9485f03b9f3493541b445",
+    "url": "./index.html"
+  },
+  {
+    "revision": "873725558534dc303b40",
+    "url": "./static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "7d6b7c9fcc6daf0250c7",
+    "url": "./static/css/main.d3faeac3.chunk.css"
+  },
+  {
+    "revision": "873725558534dc303b40",
+    "url": "./static/js/2.6fe7b9fb.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "./static/js/2.6fe7b9fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d6b7c9fcc6daf0250c7",
+    "url": "./static/js/main.332b7ec2.chunk.js"
+  },
+  {
+    "revision": "a9cc3a3180d9de5e44c8",
+    "url": "./static/js/runtime-main.fa2dd4cd.js"
+  }
+]);
